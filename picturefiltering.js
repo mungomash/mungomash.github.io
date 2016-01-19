db945bed10154a039a816e8d68bc45e6da96e4df
David Dalton <david@chaddalton.com>
2016-01-19 00:37:10 -0500

    centered the pictures individually and started the filtering

---
+++ picturefiltering.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("p").filter(".intro");
+});
+
+
+function filter() {
+	var amanda = document.getElementById("amanda").innerHTML;
+	var emma = document.getElementById("emma").innerHTML;
+	var alexandra = document.getElementById("alexandra").innerHTML;
+	var lauren = document.getElementById("lauren").innerHTML;
+	var hannah = document.getElementById("hannah").innerHTML;
+	var jonathan = document.getElementById("jonathan").innerHTML;
+
+	
+
+}
