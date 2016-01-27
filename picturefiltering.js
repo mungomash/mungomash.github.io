58e129b2dabd373a24271070577d9bff84a6ed82
David Dalton <david@chaddalton.com>
2016-01-27 12:10:55 -0500

    added the MUNGOMASH pages on the bottom in sketches

--- picturefiltering.js
+++ picturefiltering.js
@@ -1,16 +1,3 @@
-$(document).ready(function(){
-    $("p").filter(".intro");
-});
-
-
-function filter() {
-	var amanda = document.getElementById("amanda").innerHTML;
-	var emma = document.getElementById("emma").innerHTML;
-	var alexandra = document.getElementById("alexandra").innerHTML;
-	var lauren = document.getElementById("lauren").innerHTML;
-	var hannah = document.getElementById("hannah").innerHTML;
-	var jonathan = document.getElementById("jonathan").innerHTML;
-
-	
-
-}
+var view = function(person){
+    name = this.span({'class': 'name'}, person.name);
+};
