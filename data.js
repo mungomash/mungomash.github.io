cffd72d7fc9b6c0d5531a5d5c670a2b072bb0b74
Chad Dalton <mungomash@gmail.com>
2016-02-06 14:19:45 -0500

    add use handlebar template on clip_art page

---
+++ data.js
@@ -0,0 +1,17 @@
+function load_artwork(callback) {
+    loadJSON('artwork.json', function(response) {
+        callback(JSON.parse(response));
+    });
+}
+
+function loadJSON(fileName, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', fileName, true);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
