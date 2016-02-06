19c82ecba0e32b8ea9d054a28cf0c5ac9035ff7e
Chad Dalton <mungomash@gmail.com>
2016-02-06 14:34:06 -0500

    get artist name from people.json

--- data.js
+++ data.js
@@ -1,7 +1,24 @@
 function load_artwork(callback) {
-    loadJSON('artwork.json', function(response) {
-        callback(JSON.parse(response));
+    loadJSON('people.json', function(peopleResponse) {
+        people = JSON.parse(peopleResponse);
+
+        loadJSON('artwork.json', function(response) {
+            artwork = JSON.parse(response);
+
+            for (a = 0; a < artwork.length; a++) {
+                artwork[a].artist = "unknown";
+                
+                for (p = 0; p < people.length; p++) {
+                    if (artwork[a].artist_id == people[p].id) {
+                        artwork[a].artist = people[p].first_name + " " + people[p].last_name;
+                    }
+                }
+            }
+
+            callback(artwork);
+        });
     });
+
 }
 
 function loadJSON(fileName, callback) {
