795c812c3c1480779a9eaac15707e9193639eadf
David Dalton <david@chaddalton.com>
2016-02-09 11:39:19 -0500

    Use this one dad. buttons.html was modified as well

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-function load_artwork(callback) {
+function load_artwork(page, callback) {
     loadJSON('people.json', function(peopleResponse) {
         people = JSON.parse(peopleResponse);
 
@@ -6,6 +6,11 @@ function load_artwork(callback) {
             artwork = JSON.parse(response);
 
             for (a = 0; a < artwork.length; a++) {
+                if (artwork[a].page != page) {
+                    artwork.splice(a);
+                        break;
+                }
+                alert(artwork[a].url);
                 artwork[a].artist = "unknown";
                 
                 for (p = 0; p < people.length; p++) {
@@ -14,8 +19,9 @@ function load_artwork(callback) {
                     }
                 }
             }
-
+            console.log("Almost there...");
             callback(artwork);
+            console.log("Succes");
         });
     });
 
@@ -32,3 +38,36 @@ function loadJSON(fileName, callback) {
     };
     xobj.send(null);  
  }
+
+ var source = "{{#each artwork}}" +
+             "<td>" +
+             "<div class = 'panel panel-default' style='width:200px;'>" +
+                  "<div class = 'panel-body'>" +
+                       "<a href='{{url}}'><img src='{{src}}' alt='{{url}}' style='width:100%;height:100%;'></a>" +
+                  "</div>" +
+                  "<div class = 'panel-footer'>" +
+                       "{{artist}}" +
+                  "</div>" +
+              "</div>" +
+              "</td>" +
+             "{{/each}}";
+
+             
+
+var template = Handlebars.compile(source);
+
+function loadPageArtwork() {
+    load_artwork(window.location.pathname, function(list) {
+        var result = template({ artwork : list });
+        document.getElementById("clip_art_row").innerHTML = result;
+    });
+}
+
+
+
+
+
+
+
+
+
